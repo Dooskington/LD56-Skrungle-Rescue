e4f1a2b80684a122d1c346744217f1d25c3b1782
Declan Hopkins <hopkins.declan@gmail.com>
2024-10-05 15:34:39 -0700

    [?] hit impact effect

--- Assets/Source/HealthComponent.cs
+++ Assets/Source/HealthComponent.cs
@@ -1,14 +1,54 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class HealthComponent : MonoBehaviour
 {
     [SerializeField] private float _maxHealth = 3.0f;
-    
+    [SerializeField] private Material _impactEffectMaterial;
+    [SerializeField] private float _impactEffectDuration = 0.1f;
+
+    private List<Tuple<Renderer, Material>> _materials = new List<Tuple<Renderer, Material>>();
+    private Sequence _impactSequence;
+
     public float Health { get; private set; }
 
     private void Start()
     {
         Health = _maxHealth;
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            Material material = renderer.material;
+            _materials.Add(new Tuple<Renderer, Material>(renderer, material));
+        }
+
+        _impactSequence = DOTween.Sequence()
+        .AppendCallback(() =>
+        {
+            foreach (Tuple<Renderer, Material> renderer in _materials)
+            {
+                if (renderer != null)
+                {
+                    renderer.Item1.material = _impactEffectMaterial;
+                }
+            }
+        })
+        .AppendInterval(_impactEffectDuration)
+        .AppendCallback(() =>
+        {
+            foreach (Tuple<Renderer, Material> renderer in _materials)
+            {
+                if (renderer != null)
+                {
+                    renderer.Item1.material = renderer.Item2;
+                }
+            }
+        })
+        .Pause()
+        .SetAutoKill(false);
     }
 
     private void Die()
@@ -23,5 +63,9 @@ public class HealthComponent : MonoBehaviour
         {
             Die();
         }
+
+        _impactSequence.Complete();
+        _impactSequence.Rewind();
+        _impactSequence.Play();
     }
 }
